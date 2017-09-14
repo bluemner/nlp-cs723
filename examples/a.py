f82648b047129ec0310dc614bf84e1718f2b7090
Brandon Bluemner <bluemner@users.noreply.github.com>
2017-09-13 19:45:30 -0500

    sample information

---
+++ examples/a.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.data.path.append("L:\\nltk_data")
+
+from nltk.corpus import PlaintextCorpusReader
+
+text_folder = '../data/'
+wordlist = PlaintextCorpusReader(text_folder,'.*')
+print(wordlist.fileids())
+file_name ='a.txt'
+print(wordlist.words(file_name))
