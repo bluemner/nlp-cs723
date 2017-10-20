89ed915161eb2774da78bebc3e4dd67239f9682a
Beta <beatcore-org@users.noreply.github.com>
2017-10-20 18:53:19 -0500

    hw2

--- homework/one/pq2.py
+++ homework/one/pq2.py
@@ -9,18 +9,13 @@ def pos_sentence(sentence):
 	return pos;
 
 sentence=pos_sentence("many researchers two weeks both new positions")
-#[("many", "JJ"), ("researchers", "NNS"), ("two", "CD"), ("weeks", "NNS"), ("both","DT"), ("new", "JJ"), ("positions", "NNS")]
 grammer = "NP:{<CD>?<DT>?<JJ>*<NNS>+}"
 cp = nltk.RegexpParser(grammer)
 result= cp.parse(sentence)
-
-
 print (result)
 
-# the/DT receiving/VBG end/NN, assistant/NN managing/VBG editor/NN.
 sentence=pos_sentence("the receiving end assistant managing editor")
 grammer = "gerunds:{<NN>?<DT>?<VBG><NN>}"
 cp = nltk.RegexpParser(grammer)
 result= cp.parse(sentence)
-print (result)
-
+print(result)
